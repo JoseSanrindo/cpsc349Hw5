03fdbe5ddd3207656a151af42e871828fc6d384f
JoseSanrindo <takashisk@csu.fullerton.edu>
2020-10-23 15:48:35 -0700

    asdf

--- Coffeerun/scripts/validation.js
+++ Coffeerun/scripts/validation.js
@@ -1,11 +1,13 @@
 (function (window) {
     'use strict';
     var App = window.App || {};
+
     var Validation = {
         isCompanyEmail: function (email) {
-            return /.+@bignerdranch\.com$/.test(email);
-            }
+            return /.@who\,con$/.test(email);
+        }
     };
+
     App.Validation = Validation;
     window.App = App;
-   })(window);
+})(window);
