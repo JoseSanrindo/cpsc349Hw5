07446be630173477cd771f9434d98fbf8992b0cd
JoseSanrindo <takashisk@csu.fullerton.edu>
2020-10-25 18:59:19 -0700

    completed

---
+++ Coffeerun/scripts/app.js
@@ -0,0 +1,21 @@
+// ONLINE: you MUST fill in the following values 
+//               from Firebase/Firestore for your project 
+// PROJECT_ID,
+// WEB_API_KEY,
+// PROJECT_NUMBER
+//
+// go to... 
+// console.Firebase.google.com, then 
+//    click on the Gear Icon -> Project Settings -> General
+
+(function(window) {
+    'use strict';
+    window.FireBaseConfig = {
+        apiKey: "AIzaSyAL_dI7RwSBYSgH_1aHn_L2ejbcCpPRVJU",
+        authDomain: "cpsc349hw5.firebaseapp.com",
+        databaseURL: "https://cpsc349hw5.firebaseio.com",
+        projectId: "cpsc349hw5",
+        storageBucket: "cpsc349hw5.appspot.com",
+        messagingSenderId: "549527755526",
+    };
+})(window);
--- Coffeerun/scripts/main.js
+++ Coffeerun/scripts/main.js
@@ -1,68 +1,41 @@
-(function (window) { // Receives the 'window' object for use
-    'use strict'; // inside the function body;
+(function (window) {
+    'use strict';
     var FORM_SELECTOR = '[data-coffee-order="form"]';
-    var CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]';
-    var SERVER_URL = 'http://coffeerun-v2-rest-api.herokuapp.com/api/coffeeorders';
-
+    var CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]';     // CHOOSE ONLY ONE...
+    // var SERVER_URL = 'https://co.audstanley.com/coffeeorders';    // if running on the shared server
+    // var SERVER_URL = 'http://localhost:3000/coffeeorders';          // if running locally
 
     var App = window.App;
     var Truck = App.Truck;
-    var DataStore = App.DataStore;
-    var RemoteDataStore = App.RemoteDataStore;
+    //var DataStore = new App.DataStore;
+    //var RemoteDataStore = App.RemoteDataStore;
+    var FireBaseDataStore = App.FireBaseDataStore;
     var FormHandler = App.FormHandler;
     var Validation = App.Validation;
     var CheckList = App.CheckList;
-    var remoteDS = new RemoteDataStore(SERVER_URL);
-    var webshim = window.webshim;
+    // var remoteDS = new RemoteDataStore(SERVER_URL);
 
-    var myTruck = new Truck('ncc-1701', new DataStore());
-    window.myTruck = myTruck;
+    // do NOT create an anonymous FireBaseDataStore() -- google will not allow it
+    //    make a variable named remoteFireBase, and use that to create the new Truck
+    var remoteFireBase = new FireBaseDataStore();
+
+    // var truck = new Truck('ncc-1701', new DataStore());
+    // var truck = new Truck('ncc-1701', remoteFireBase);
+    var truck = new Truck('ncc-1701', remoteFireBase);
+    window.truck = truck;
     var checkList = new CheckList(CHECKLIST_SELECTOR);
-    checkList.addClickHandler(myTruck.deliverOrder.bind(myTruck));
+    checkList.addClickHandler(truck.deliverOrder.bind(truck));
     var formHandler = new FormHandler(FORM_SELECTOR);
 
-
-    formHandler.addSubmitHandler(function (data) {
-        return myTruck.createOrder.call(myTruck, data)
-            .then(function () {
-                    checkList.addRow.call(checkList, data);
-                },
-                function () {
-                    alert('Server unreachable. Try again later');
-                }
-            );
+    formHandler.addSubmitHandler(function(data) {
+        return truck.createOrder.call(truck, data)
+            .then(() => {
+                checkList.addRow.call(checkList, data);
+            }); 
     });
+    console.log(formHandler);
 
     formHandler.addInputHandler(Validation.isCompanyEmail);
+    truck.printOrders(checkList.addRow.bind(checkList));
 
-    myTruck.printOrders(checkList.addRow.bind(checkList));
-
-    webshim.polyfill('forms forms-ext');
-    webshim.setOptions('forms', {
-        addValidators: true,
-        lazyCustomMessages: true
-    });
 })(window);
-
-
-// Range Slider function --> Shows range on slide
-
-var rangeSlider = function () {
-    var slider = $('.range-slider');
-    var range = $('.range-slider__range');
-    var value = $('.range-slider__value');
-
-    slider.each(function () {
-
-        value.each(function () {
-            var value = $(this).prev().attr('value');
-            $(this).html(value);
-        });
-
-        range.on('input', function () {
-            $(this).next(value).html(this.value);
-        });
-    });
-};
-
-rangeSlider();
